c0fd401304e267837de06fc57c209bb41702eadc
leaderway <lidehui@m.scnu.edu.cn>
2017-03-08 10:33:58 +0800

    层次聚类抽取概念分类关系：使用全局平均相似度抽取父概念

--- src/main/java/org/leader/data/relation/HierarchicalClustering.java
+++ src/main/java/org/leader/data/relation/HierarchicalClustering.java
@@ -1,12 +1,12 @@
 package org.leader.data.relation;
 
-import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.LineIterator;
 import org.leader.framework.helper.DatabaseHelper;
 import org.leader.framework.util.CollectionUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import sun.management.VMManagement;
 
 import java.io.File;
 import java.util.*;
@@ -24,6 +24,7 @@ public class HierarchicalClustering {
     private static final String VSM_PATH = "./data/combineWordVSM.txt";//  记录词语向量空间模型
     private static final String CLUSTER_RESULT_PATH = "./data/JULEIcombineWordVSM.txt";//  记录聚类后词语向量空间模型
     private static final double MI_EXTRACTION_WORD_MI_THRESHOLD = 0.00032651390174099563;// 基于统计方法得到的词语抽取阈值
+    private static final double CLUSTER_THRESHOLD = 0.05;// 层次聚类相似度阈值
 
     public static void main(String[] args) throws Exception{
         Set<String> conceptWordSet = new HashSet<>();// 保存候选概念的set
@@ -66,8 +67,6 @@ public class HierarchicalClustering {
         // 构造向量空间模型
         Map<String, Double> vsmMap = new TreeMap<String, Double>();// 保存向量空间模型:组合词对：相似度
         Map<String, Double> vsmSingleMap = new TreeMap<String, Double>();// 保存向量空间模型(去重):组合词对：相似度
-        double firstMaxSim = 0.0d;
-        String maxSimWordPair = "";
         if (CollectionUtil.isNotEmpty(combineWordSingleArticleTFIDFMap)) {
             for (String combineWord : conceptWordSet) {
                 for (String combineWordOther : conceptWordSet) {
@@ -106,10 +105,7 @@ public class HierarchicalClustering {
                     }
                     // 计算余弦距离
                     double similarity = sumCombineWordMultOther / (Math.pow(combineWordSq * combineWordOtherSq, 0.5d));
-                    if (similarity != 1 && similarity > firstMaxSim) {
-                        firstMaxSim = similarity;
-                        maxSimWordPair = vsmName;
-                    }
+
                     vsmSingleMap.put(vsmName, similarity);
                     vsmMap.put(vsmName, similarity);
                     vsmMap.put(vsmNameReverse, similarity);
@@ -121,8 +117,7 @@ public class HierarchicalClustering {
             FileUtils.writeLines(new File(VSM_PATH), vsmMapList);
         }
         Map<String, Double> afterClusterSingleVSMMap = vsmSingleMap;// 聚类后重新构建去重空间向量模型，第一次与vsmSingleMap相同
-        hieCluster(conceptWordSet,afterClusterSingleVSMMap, vsmSingleMap, 0.05);
-        LOGGER.info("构建向量空间模型完成,最大的相似度是：{}，词组为{}",firstMaxSim, maxSimWordPair);
+        hieCluster(conceptWordSet,afterClusterSingleVSMMap, vsmSingleMap, CLUSTER_THRESHOLD);
     }
 
     /**
@@ -138,9 +133,9 @@ public class HierarchicalClustering {
         // 计算已经聚成的簇与剩余的词语的相似度，使用簇中每个词语与其他词语的平均相似度
         Map<String, Double> afterClusterVSMMap = new TreeMap<String, Double>();// 聚类后重新构建空间向量模型
         afterClusterSingleVSMMap = new HashMap<>();// 聚类后重新构建去重空间向量模型
-        double maxSim = 0.0;
-        String maxSimCombineWord = "";
-        String maxSimCombineWordOther = "";
+        double maxSim = 0.0;// 最大相似度
+        String maxSimCombineWord = "";// 拥有最大相似度的两个簇
+        String maxSimCombineWordOther = "";// 拥有最大相似度的两个簇
 
         for (String combineWord : conceptWordSet) {
             for (String combineWordOther : conceptWordSet) {
@@ -192,12 +187,7 @@ public class HierarchicalClustering {
         System.out.println("相似度最大的组合词对" + maxSimCombineWord + "|" + maxSimCombineWordOther + " 相似度为：" + maxSim);
         if (maxSim < simThreshold) {
             System.out.println("层次聚类结束");
-            Set<String> clusterResult = new HashSet<String>();
-            for (String cluster : conceptWordSet) {
-                if (cluster.indexOf("|") != -1) {
-                    clusterResult.add(cluster);
-                }
-            }
+            Set<String> clusterResult = findParentConcept(vsmSingleMap, conceptWordSet);
             FileUtils.writeLines(new File(CLUSTER_RESULT_PATH), clusterResult);
             return;// 结束执行
         }
@@ -206,4 +196,48 @@ public class HierarchicalClustering {
 
     }
 
+    /**
+     * 查找相似度最大的组合词对
+     * @param vsmMap
+     * @return
+     */
+    private static Set<String> findParentConcept(Map<String, Double> vsmMap, Set<String> conceptWordSet) throws Exception{
+        Set<String> resultSet = new HashSet<String>();
+        for (String result : conceptWordSet) {
+            if (result.indexOf("|") != -1) {
+                String maxSimCombineWord= "";
+                double simMax = 0.0;
+                String resultArray[] = result.split("\\|");
+                for (String resultWord : resultArray) {
+                    double simSum = 0.0d;// 全局相似度总和
+
+                    for (String resultOtherWord : resultArray) {
+                        if (!resultWord.equals(resultOtherWord)) {
+                            String vmsName = resultWord + "|" + resultOtherWord;
+                            String vmsReverseName = resultOtherWord + "|" + resultWord;
+                            double sim = 0;
+                            if (vsmMap.containsKey(vmsName)) {
+                                sim = vsmMap.get(vmsName);
+                            } else if (vsmMap.containsKey(vmsReverseName)) {
+                                sim = vsmMap.get(vmsReverseName);
+                            } else {
+                                System.out.println("vsmMap不存在key" + vmsName);
+                            }
+                            simSum += sim;
+                        }
+                    }
+
+                    double simAvg = simSum / resultArray.length;
+                    if (simAvg != 1 && simAvg > simMax) {
+                        simMax = simAvg;
+                        maxSimCombineWord = resultWord;
+                    }
+                }
+                System.out.println("全局相似度最大的组合词" + maxSimCombineWord + " 相似度为：" + simMax);
+                resultSet.add(maxSimCombineWord + "-" + result);
+            }
+        }
+
+        return resultSet;
+    }
 }
